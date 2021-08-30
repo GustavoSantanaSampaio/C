8e5f7b98710797b7432a37aceafb5147ebdf470a
GustavoSantanaSampaio <89815071+GustavoSantanaSampaio@users.noreply.github.com>
2021-08-30 19:50:00 -0300

    Add files via upload

---
+++ Ex11.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Função principal do programa
+int main(int argc, char *argv[]){
+
+  // Declaração de variáveis
+  int V1, V2;
+
+  // Solicita entrada de dados para o usuário e grava o valor digitado em uma variável "a"
+  printf("Digite o primeiro valor: ");
+  scanf("%i", &V1);
+
+  printf("Digite o segundo valor: ");
+  scanf("%i", &V2);
+
+  if(V2>V1){
+      printf("O maior valor digitado foi %i", V2);
+    }  
+  else if(V1>V2){
+     printf("O maior valor digitado foi %i", V1);
+  }
+  else{
+      printf("Os valores digitados sao iguais.");
+  }
+  return 0;
+}
