3a7508a1f64f7389efb839e5b2cb8086d02c0b8e
GustavoSantanaSampaio <89815071+GustavoSantanaSampaio@users.noreply.github.com>
2021-11-08 21:50:22 -0300

    Delete Ex05.py

--- Ex05.py
+++
@@ -1,5 +0,0 @@
-temperatura = float(input("Digite o valor da temperatura em graus Celsius:  "))
- 
-resultado = (temperatura * 1.8) +32
-
-print(f"O valor da temperatura em Fahrenheit é {resultado} ") 
