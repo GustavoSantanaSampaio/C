a5e534345303daa96674ab693732365557db8cd4
GustavoSantanaSampaio <89815071+GustavoSantanaSampaio@users.noreply.github.com>
2021-11-08 21:55:29 -0300

    Delete Ex22.py

--- Ex22.py
+++
@@ -1,7 +0,0 @@
-print('Digite um numero positivo')
- 
-num = int(input("Digite um numero: "))
- 
-while(num <= 0):
-    print("Não pode numero negativo!")
-    num = int(input("Digite um outro numero:"))
