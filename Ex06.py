f39a7e21566bc95b45f3ee9002a9a89db3639d13
GustavoSantanaSampaio <89815071+GustavoSantanaSampaio@users.noreply.github.com>
2021-11-08 21:50:32 -0300

    Delete Ex06.py

--- Ex06.py
+++
@@ -1,6 +0,0 @@
-cotacao = float(input("Digite o valor da cotação do dólar:  "))
-valor = float(input("Digite o valor em dólar:  "))
- 
-resultado = valor * cotacao
-
-print(f"O valor em reais é {resultado} ") 
