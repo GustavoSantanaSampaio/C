23329134263914c032e334a6fd0bbe228ce0a0c9
GustavoSantanaSampaio <89815071+GustavoSantanaSampaio@users.noreply.github.com>
2021-11-08 21:48:55 -0300

    Delete Ex01.py

--- Ex01.py
+++
@@ -1,6 +0,0 @@
-base = float(input("Digite o valor da base:  "))
-altura = float(input("Digite o valor da altura: "))
- 
-resultado = base * altura
-
-print(f"O valor da area do triângulo é {resultado} ")
