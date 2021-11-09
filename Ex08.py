d995d91c08ed875eca75a883a32b01da2f208af2
GustavoSantanaSampaio <89815071+GustavoSantanaSampaio@users.noreply.github.com>
2021-11-08 21:50:51 -0300

    Delete Ex08.py

--- Ex08.py
+++
@@ -1,6 +0,0 @@
-peso = float(input("Digite o valor do peso:  "))
-altura = float(input("Digite o valor da altura:  "))
- 
-resultado = peso/(altura*altura)
-
-print(f"O valor do imc é {resultado} ") 
