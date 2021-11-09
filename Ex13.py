44e6ee463e8963a3037a06d3bd763f65a508269d
GustavoSantanaSampaio <89815071+GustavoSantanaSampaio@users.noreply.github.com>
2021-11-08 21:52:43 -0300

    Delete Ex13.py

--- Ex13.py
+++
@@ -1,8 +0,0 @@
-base = float(input("Digite o valor da base: "))
-altura = float(input("Digite o valor da altura: "))
-
-area = base * altura
-if (area > 100):
-    print("Terreno grande.")
-else:
-    print("Terreno pequeno.")
