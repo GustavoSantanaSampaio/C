5e97ea94eb37c1ad105ece9bc2370b602bca0ef1
GustavoSantanaSampaio <89815071+GustavoSantanaSampaio@users.noreply.github.com>
2021-11-08 21:55:39 -0300

    Delete Ex23.py

--- Ex23.py
+++
@@ -1,6 +0,0 @@
-num1 = float(input("Digite o primeiro valor:  "))
-num2 = float(input("Digite o segundo valor: "))
-
-while(num1>=num2):
-    print("Erro! numero 2 precisa ser maior.\n")
-    num2 = int(input("Digite o numero 2 novamente: \n"))
