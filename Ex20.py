a2b6de07eeb7ba20cb369bd09cb69b8b63614482
GustavoSantanaSampaio <89815071+GustavoSantanaSampaio@users.noreply.github.com>
2021-11-08 21:55:01 -0300

    Delete Ex20.py

--- Ex20.py
+++
@@ -1,9 +0,0 @@
-p1 = float(input("Digite o valor da p1:  "))
-p2 = float(input("Digite o valor da p2: "))
- 
-media = (p1+2*p2)/3
-
-if(media>=5):
-    print("Aprovado.")
-else:
-    print("Reprovado.")
